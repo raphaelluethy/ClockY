86382f2dea937fe69873749b3149974e0007edd6
Raphael Lüthy <raphael@famluethy.chraphael@famluethy.chraphael@famluethy.ch>
2022-07-04 14:13:41 +0200

    changed db back to sqlite

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,7 +5,7 @@ import { signIn, signOut, useSession } from "next-auth/react";
 
 const Home: NextPage = () => {
 	const hello = trpc.useQuery(["example.hello", { text: "from Clocky" }]);
-	const session = useSession();
+	const { data: session, status } = useSession();
 
 	return (
 		<>
@@ -18,25 +18,36 @@ const Home: NextPage = () => {
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
 
-			<div className="bg-slate-600 flex justify-center h-screen w-screen items-center">
+			<div className="bg-slate-600 grid grid-cols-1 content-center h-screen w-screen items-center text-center">
 				<div className="py-6  text-slate-50">
 					{hello.data ? (
 						<h1 className="text-4xl">{hello.data.greeting}</h1>
 					) : (
 						<p>Loading..</p>
 					)}
-					<h2 className="text-2xl text-center">Comming Soon...</h2>
+					<h2 className="text-2xl">Comming Soon...</h2>
 				</div>
-				{session.status === "authenticated" ? (
-					<div>
-						<p className="text-center">
-							{" "}
-							You are logged in as {JSON.stringify(session?.data)}
+				{!!session ? (
+					<div className="text-slate-50">
+						<p className="underline decoration-slate-100 text-lg text-slate-50 hover:text-red-500 hover:decoration-red-500 hover:font-bold">
+							You are logged in as {session.user?.email}
 						</p>
-						<button onClick={() => signOut()}>Sign out</button>
+						<button
+							className="bg-transparent hover:bg-slate-50 text-slate-50 font-semibold hover:text-slate-600 py-2 px-4 border border-slate-50 hover:border-transparent rounded my-4"
+							onClick={() => signOut()}
+						>
+							Sign out
+						</button>
 					</div>
 				) : (
-					<button onClick={() => signIn()}>Sign in</button>
+					<div>
+						<button
+							className="bg-transparent hover:bg-slate-50 text-slate-50 font-semibold hover:text-slate-600 py-2 px-4 border border-slate-50 hover:border-transparent rounded"
+							onClick={() => signIn()}
+						>
+							Sign in
+						</button>
+					</div>
 				)}
 			</div>
 		</>
