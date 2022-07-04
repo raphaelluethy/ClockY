77c7acc1998f3dc899f7bf3bf8218ee25cbc52fb
Raphael Lüthy <raphael@famluethy.chraphael@famluethy.chraphael@famluethy.ch>
2022-07-04 11:22:08 +0200

    created boilerplate app

---
+++ src/pages/index.tsx
@@ -0,0 +1,33 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import { trpc } from "../utils/trpc";
+
+const Home: NextPage = () => {
+	const hello = trpc.useQuery(["example.hello", { text: "from Clocky" }]);
+
+	return (
+		<>
+			<Head>
+				<title>ClockY</title>
+				<meta
+					name="description"
+					content="Another try to build a wepapp"
+				/>
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
+
+			<div className="bg-slate-600 flex justify-center h-screen w-screen items-center">
+				<div className="py-6  text-slate-50">
+					{hello.data ? (
+						<h1 className="text-4xl">{hello.data.greeting}</h1>
+					) : (
+						<p>Loading..</p>
+					)}
+					<h2 className="text-2xl text-center">Comming Soon...</h2>
+				</div>
+			</div>
+		</>
+	);
+};
+
+export default Home;
